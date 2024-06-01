3ecc3a51985162cac97786552575da4b33d5e110
Anthony Tossell <atossell91@gmail.com>
2024-05-31 17:27:55 -0700

    SQL for Activity and InspectorActivity table

--- create-db.sh
+++ create-db.sh
@@ -1,5 +1,7 @@
 sqlite3 MRRS.db <<EOF
 .read ./SQL/create-inspector-table.sql
+.read ./SQL/create-activity-table.sql
+.read ./SQL/create-inspector-activity-table.sql
 INSERT INTO Inspector (FirstName, LastName) VALUES ('Rubber', 'Duck');
 DELETE FROM Inspector;
 SELECT * FROM RecordOfInspectorChanges;
